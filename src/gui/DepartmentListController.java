4c659af289000494f9b347d7a544cd24c349da87
Gustavo Pereira <guspereira2010@gmail.com>
2020-05-03 10:47:25 -0300

    DepartmentList Controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,52 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable{
+	
+	@FXML
+	private Button btNew;
+	
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	public void onBtNewAction() {
+		//SELECT * FROM department;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// para manipular a tabela
+		initializeNodes();
+		
+	}
+
+	//método tabela inicia o comprotamento das colunas.
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		//faz com que a table ocupe todo o Stage.
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+}
---
+++ src/model/entities/Department.java
@@ -0,0 +1,76 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Department implements Serializable {
+
+	private Integer id;
+	private String name;
+
+	public Department() {
+	}
+
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	// get
+	public String getName() {
+		return name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	// set
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+
+	public boolean equals(Object obj) {
+
+		if (this == obj)
+
+			return true;
+
+		if (obj == null)
+
+			return false;
+
+		if (getClass() != obj.getClass())
+
+			return false;
+
+		Department other = (Department) obj;
+
+		if (id == null) {
+
+			if (other.id != null)
+
+				return false;
+
+		} else if (!id.equals(other.id))
+
+			return false;
+
+		return true;
+
+	}
+
+	@Override
+
+	public String toString() {
+
+		return "Department [id=" + id + ", name=" + name + "]";
+
+	}
+
+}
